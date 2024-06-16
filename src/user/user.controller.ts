85154a753db07b6da97b754e3853c17660f23800
Aditya170700 <adityaric72@gmail.com>
2024-06-16 21:30:37 +0700

    HTTP Param using decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 
 @Controller('/api/users')
 export class UserController {
@@ -10,5 +10,18 @@ export class UserController {
   @Get("/sample")
   get(): string {
     return "GET";
+  }
+
+  @Get("/hello")
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string
+  ): string {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get("/:id")
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
   }
 }
