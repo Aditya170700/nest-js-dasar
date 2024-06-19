51aae7db2199fc073f89dd83a8375c9608a20668
Aditya170700 <adityaric72@gmail.com>
2024-06-20 06:17:44 +0700

    Logger

--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,19 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma/prisma.service';
 import { User } from '@prisma/client';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class UserRepository {
-  constructor(private prismaService: PrismaService) {
-    console.log('Create User Repository');
+  constructor(
+    private prismaService: PrismaService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+  ) {
+    this.logger.info('Create User Repository');
   }
 
   async save(firstName: string, lastName?: string): Promise<User> {
-    return await this.prismaService.user.create({
+    const user = await this.prismaService.user.create({
       data: {
         first_name: firstName,
         last_name: lastName,
       }
     });
+
+    this.logger.info(`Create User Data ${JSON.stringify(user)}`);
+
+    return user;
   }
 }
